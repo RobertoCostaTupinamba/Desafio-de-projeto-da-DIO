8454b1f5243f5d2dba4f82c23c73eb97d347c211
Roberto Tupinamba <robertotupinamba@gmail.com>
2021-09-13 17:06:30 -0300

    Primeiro projeto com .NET

---
+++ PrimeiroProjetoComDotNETConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Desafio_de_projeto_da_DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 5;
+
+            for (int i = 0; i < numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Ola mundo com .NET {i}");
+            }
+        }
+    }
+}
